8f49a08860e732d0b9ddeb91bd497ad743218df7
Lior Amram <lior@adap.tv>
2012-06-06 13:37:16 -0700

    http_client fixes and improvements
    Change-Id: If1a407289bb446b84be392e6eb1fce909da45f8a

--- src/http_client_pool.c
+++ src/http_client_pool.c
@@ -28,14 +28,21 @@ int http_client_pool_init(struct http_client_pool *http_client_pool, size_t init
         return;                                          \
     }
 
-#define READ_DATA(cond)                                     \
+#define _READ_MORE_DATA(cond, extra)                        \
     while(cond) {                                           \
         if (res == 0)                                       \
             CLIENT_ERROR(); /* partial response */          \
         yield();                                            \
         if ((res = vmbuf_read(&ctx->response, fd)) < 0)     \
             CLIENT_ERROR();                                 \
+        extra;                                              \
     }
+
+#define READ_MORE_DATA_STR(cond)                                \
+    _READ_MORE_DATA(cond, *vmbuf_wloc(&ctx->response) = 0)
+
+#define READ_MORE_DATA(cond)                    \
+    _READ_MORE_DATA(cond, )
 
 
 void http_client_fiber_main(void) {
@@ -57,8 +64,9 @@ void http_client_fiber_main(void) {
     uint32_t eoh_ofs;
     char *data;
     char *eoh;
-    res = 1; /* READ_DATA checks res */
-    READ_DATA(NULL == (eoh = strstr(data = vmbuf_data(&ctx->response), CRLFCRLF)));
+    res = vmbuf_read(&ctx->response, fd);
+    *vmbuf_wloc(&ctx->response) = 0;
+    READ_MORE_DATA_STR(NULL == (eoh = strstr(data = vmbuf_data(&ctx->response), CRLFCRLF)));
     eoh_ofs = eoh - data + SSTRLEN(CRLFCRLF);
     *eoh = 0;
     char *p = strstr(data, CONNECTION);
@@ -81,27 +89,22 @@ void http_client_fiber_main(void) {
     if (NULL != content_len) {
         content_len += SSTRLEN(CONTENT_LENGTH);
         size_t content_end = eoh_ofs + atoi(content_len);
-        READ_DATA(vmbuf_wlocpos(&ctx->response) < content_end);
+        READ_MORE_DATA(vmbuf_wlocpos(&ctx->response) < content_end);
     } else {
         char *transfer_encoding_str = strstr(data, TRANSFER_ENCODING);
         if (NULL != transfer_encoding_str &&
             0 == SSTRNCMP(transfer_encoding_str + SSTRLEN(TRANSFER_ENCODING), "chunked")) {
-#if 0
-
             size_t chunk_start = eoh_ofs;
-            data = vmbuf_data(&ctx->response);
-            *vmbuf_wloc(&ctx->response) = 0;
-            char *chunk_size = data + chunk_start;
             /*
               <chunk size in hex>\r\n
               <..... chunk data .....>
               chunk size == 0 is end of chunks
             */
             char *p;
-            READ_DATA((p = strchrnul(chunk_size, '\r')) == 0);
-
-#endif
-
+            READ_MORE_DATA_STR((p = strchrnul((data = vmbuf_data(&ctx->response)) + chunk_start, '\r')) == 0);
+            uint32_t s = strtoul(data + chunk_start, NULL, 16);
+            chunk_start = p - data + SSTRLEN(CRLF);
+            (void)s;
         } else {
             for (;;yield()) {
                 if ((res = vmbuf_read(&ctx->response, fd)) < 0)
