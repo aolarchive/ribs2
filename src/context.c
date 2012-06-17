56513f3234e43050c95bdc59556e6a6155353ea3
Lior Amram <lioramr@gmail.com>
2012-06-17 11:34:13 -0700

    simplified ribs_context_create
    Change-Id: I4a8440a6617d52176d6706ed3984a73fd642aac8

--- src/context.c
+++ src/context.c
@@ -4,9 +4,6 @@
 struct ribs_context main_ctx;
 struct ribs_context *current_ctx = &main_ctx;
 
-static void *last_stack = NULL;
-static size_t last_stack_size = 0;
-
 int ribs_makecontext(struct ribs_context *ctx, struct ribs_context *rctx, void *sp, void (*func)(void)) {
     /* align stack to 16 bytes, assuming function always does push rbp to align
        __ribs_context_exit doesn't need to be aligned since it doesn't rely on stack alignment
@@ -25,38 +22,20 @@ int ribs_makecontext(struct ribs_context *ctx, struct ribs_context *rctx, void *
     return 0;
 }
 
-struct fiber_wrapper {
-    void (*func)();
-    size_t stack_size;
-};
-
-static void __ribs_fiber_wrapper() {
-    struct fiber_wrapper *fw = (struct fiber_wrapper *)current_ctx->reserved;
-    fw->func();
-    void *stack = current_ctx;
-    stack -= fw->stack_size;
-    free(last_stack);
-    last_stack = stack;
-    last_stack_size = fw->stack_size;
-}
-
 struct ribs_context *ribs_context_create(size_t stack_size, void (*func)(void)) {
     void *stack;
-    if (last_stack_size == stack_size) {
-        stack = last_stack; /* reuse stack if possible */
-    } else {
-        free(last_stack);
-        stack = malloc(stack_size + sizeof(struct ribs_context) + sizeof(struct fiber_wrapper));
-    }
-    last_stack = NULL; /* last stack was reused or freed */
-    last_stack_size = 0;
-    if (NULL == stack)
-        return NULL;
+    stack = malloc(stack_size + sizeof(struct ribs_context) + sizeof(size_t));
     stack += stack_size;
     struct ribs_context *ctx = (struct ribs_context *)stack;
-    ribs_makecontext(ctx, current_ctx, ctx, __ribs_fiber_wrapper);
-    struct fiber_wrapper *fw = (struct fiber_wrapper *)ctx->reserved;
-    fw->func = func;
-    fw->stack_size = stack_size;
+    ribs_makecontext(ctx, current_ctx, ctx, func);
+    size_t *stack_size_stored = (size_t *)ctx->reserved;
+    *stack_size_stored = stack_size;
     return ctx;
+}
+
+void ribs_context_free(struct ribs_context *ctx) {
+    void *stack = current_ctx;
+    size_t *stack_size_stored = (size_t *)ctx->reserved;
+    stack -= *stack_size_stored;
+    free(stack);
 }
