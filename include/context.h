693316dd3be4e9276428b6cd88be82615688c6d5
Nir Yeffet <nir@adap.tv>
2012-06-18 12:03:40 -0700

    void
    Change-Id: I314e5238999f3d9d027f437bfb61d1c7a1a65470

--- include/context.h
+++ include/context.h
@@ -21,8 +21,8 @@ struct ribs_context {
 extern struct ribs_context main_ctx;
 extern struct ribs_context *current_ctx;
 
-extern int ribs_swapcurcontext(struct ribs_context *rctx);
-extern int ribs_makecontext(struct ribs_context *ctx, struct ribs_context *rctx, void *sp, void (*func)(void));
+extern void ribs_swapcurcontext(struct ribs_context *rctx);
+extern void ribs_makecontext(struct ribs_context *ctx, struct ribs_context *rctx, void *sp, void (*func)(void));
 extern void __ribs_context_exit(void);
 
 extern struct ribs_context *ribs_context_create(size_t stack_size, void (*func)(void));
--- src/context.c
+++ src/context.c
@@ -4,7 +4,7 @@
 struct ribs_context main_ctx;
 struct ribs_context *current_ctx = &main_ctx;
 
-int ribs_makecontext(struct ribs_context *ctx, struct ribs_context *rctx, void *sp, void (*func)(void)) {
+void ribs_makecontext(struct ribs_context *ctx, struct ribs_context *rctx, void *sp, void (*func)(void)) {
     /* align stack to 16 bytes, assuming function always does push rbp to align
        __ribs_context_exit doesn't need to be aligned since it doesn't rely on stack alignment
        (needed when using SSE instructions)
@@ -18,8 +18,6 @@ int ribs_makecontext(struct ribs_context *ctx, struct ribs_context *rctx, void *
 
     ctx->rsp = (uintptr_t) sp;
     ctx->rbx = (uintptr_t) rctx;
-
-    return 0;
 }
 
 struct ribs_context *ribs_context_create(size_t stack_size, void (*func)(void)) {
