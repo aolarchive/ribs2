defb7e79113d0482dba60268e6b5cbc5f1271287
Carlo Marcelo Arenas Belon <carlo@adap.tv>
2012-06-22 12:55:42 -0700

    use pthread also for preprocessor
    Change-Id: Idac2152932a40cc527e09894b11571fd657be6dc

--- src/test_mysql.mk
+++ src/test_mysql.mk
@@ -3,7 +3,7 @@ TARGET=test_mysql
 SRC=test_mysql.c
 
 CFLAGS+= -I ../include -I../3rd_party/mysql_client/include
-LDFLAGS+= -Wl,-static -lmysqlclient -Wl,-dy -lm -lpthread -ldl -lz
+LDFLAGS+= -Wl,-static -lmysqlclient -Wl,-dy -lm -pthread -ldl -lz
 LIBS+=ribs2
 
 include ../make/ribs.mk
