cd6c4440963e3fc67971d74abe336d6257423602
Lior Amram <lior@adap.tv>
2012-07-23 11:44:03 -0700

    added LGPL2.1 license
    Change-Id: I1a71e077b7a932cbda0056476fed8d4f786a315a

--- src/_timeout_handler.c
+++ src/_timeout_handler.c
@@ -1,4 +1,25 @@
+/*
+    This file is part of RIBS2.0 (Robust Infrastructure for Backend Systems).
+    RIBS is an infrastructure for building great SaaS applications (but not
+    limited to).
 
+    Copyright (C) 2012 Adap.tv, Inc.
+
+    RIBS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, version 2.1 of the License.
+
+    RIBS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with RIBS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
+ * inline
+ */
 _RIBS_INLINE_ void timeout_handler_add_fd_data(struct timeout_handler *timeout_handler, struct epoll_worker_fd_data *fd_data) {
     gettimeofday(&fd_data->timestamp, NULL);
     if (list_empty(&timeout_handler->timeout_chain)) {
