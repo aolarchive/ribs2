eb59c011db688d6eb82b5bd9bdd244833a8c1c21
Nishant Trivedi <nishant@adap.tv>
2012-09-05 16:44:58 -0700

    added binary search
    Change-Id: I9bc2c9577edd6eb3cf1c8ea9dec429c618ddf56f

---
+++ include/search.h
@@ -0,0 +1,27 @@
+/*
+    This file is part of RIBS (Robust Infrastructure for Backend Systems).
+    RIBS is an infrastructure for building great SaaS applications (but not
+    limited to).
+
+    Copyright (C) 2011 Adap.tv, Inc.
+
+    RIBS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, version 2.1 of the License.
+
+    RIBS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with RIBS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef _SEARCH__H_
+#define _SEARCH__H_
+
+#include <unistd.h>
+
+const void *binary_search(const void *key, const void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *));
+
+#endif // _SEARCH__H_
---
+++ src/search.c
@@ -0,0 +1,49 @@
+/*
+    This file is part of RIBS (Robust Infrastructure for Backend Systems).
+    RIBS is an infrastructure for building great SaaS applications (but not
+    limited to).
+
+    Copyright (C) 2011 Adap.tv, Inc.
+
+    RIBS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, version 2.1 of the License.
+
+    RIBS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with RIBS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "search.h"
+
+const void *binary_search(const void *key, const void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *)) {
+
+    size_t low = 0, high = nmemb;
+
+    while (low < high)
+    {
+        size_t curr = (low + high) >> 1;
+        const void *memb = base + (size * curr);
+
+        if(0 < compar(key, memb)) {
+            low = curr;
+            ++low;
+        } else{
+            high = curr;
+        }
+    }
+
+    if (low >= nmemb)
+        return NULL;
+
+    const void *res = base + (size * low);
+    if(0 != compar(key, res))
+        return NULL;
+
+    return res;
+}
+
