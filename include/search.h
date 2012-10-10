aecfa3ce358bd43238c0cb58c44664e0f45bbe0c
Carlo Marcelo Arenas Belon <carlo@adap.tv>
2012-10-10 00:27:08 -0700

    update license header
    Change-Id: I78e90c50708d6eeb51664aeed1ba7f347501ef26

--- include/search.h
+++ include/search.h
@@ -1,9 +1,9 @@
 /*
-    This file is part of RIBS (Robust Infrastructure for Backend Systems).
+    This file is part of RIBS2.0 (Robust Infrastructure for Backend Systems).
     RIBS is an infrastructure for building great SaaS applications (but not
     limited to).
 
-    Copyright (C) 2011 Adap.tv, Inc.
+    Copyright (C) 2011,2012 Adap.tv, Inc.
 
     RIBS is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
