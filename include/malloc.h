e061bca885eb46e3fee897a9592c69171cd4da1f
Lior Amram <lior@adap.tv>
2013-04-12 11:09:54 -0700

    sprintf, strdup, memdup in ribs malloc

--- include/malloc.h
+++ include/malloc.h
@@ -8,6 +8,11 @@
 _RIBS_INLINE_ void *ribs_malloc(size_t size);
 _RIBS_INLINE_ void *ribs_calloc(size_t nmemb, size_t size);
 _RIBS_INLINE_ void ribs_reset_malloc(void);
+_RIBS_INLINE_ char *ribs_malloc_vsprintf(const char *format, va_list ap);
+_RIBS_INLINE_ char *ribs_malloc_sprintf(const char *format, ...);
+_RIBS_INLINE_ void *ribs_memdup(const void *s, size_t n);
+_RIBS_INLINE_ char *ribs_strdup(const char *s);
+_RIBS_INLINE_ char *ribs_malloc_strftime(const char *format, const struct tm *tm);
 
 #include "../src/_malloc.c"
 
--- src/_malloc.c
+++ src/_malloc.c
@@ -13,3 +13,27 @@ _RIBS_INLINE_ void *ribs_calloc(size_t nmemb, size_t size) {
 _RIBS_INLINE_ void ribs_reset_malloc(void) {
     memalloc_reset(&current_ctx->memalloc);
 }
+
+_RIBS_INLINE_ char *ribs_malloc_vsprintf(const char *format, va_list ap) {
+    return memalloc_vsprintf(&current_ctx->memalloc, format, ap);
+}
+
+_RIBS_INLINE_ char *ribs_malloc_sprintf(const char *format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    char *str = ribs_malloc_vsprintf(format, ap);
+    va_end(ap);
+    return str;
+}
+
+_RIBS_INLINE_ void *ribs_memdup(const void *s, size_t n) {
+    return memalloc_memcpy(&current_ctx->memalloc, s, n);
+}
+
+_RIBS_INLINE_ char *ribs_strdup(const char *s) {
+    return memalloc_strcpy(&current_ctx->memalloc, s);
+}
+
+_RIBS_INLINE_ char *ribs_malloc_strftime(const char *format, const struct tm *tm) {
+    return memalloc_strftime(&current_ctx->memalloc, format, tm);
+}
