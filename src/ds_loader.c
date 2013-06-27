22aa2ef202a9dca16a33a960cc84156f6fc5667a
Lior Amram <lior@adap.tv>
2013-06-27 15:40:40 -0700

    reduce verbosity

--- src/ds_loader.c
+++ src/ds_loader.c
@@ -5,13 +5,12 @@
 int ds_loader_verify_files(const char *path, const char **files) {
     LOGGER_INFO("verifying files...");
     const char **fn = files;
+    char fname[PATH_MAX];
     for (;*fn;++fn) {
-        char fname[PATH_MAX];
         if (PATH_MAX <= snprintf(fname, PATH_MAX, "%s/%s", path, *fn))
             return LOGGER_ERROR("filename too long: %.*s", PATH_MAX, fname), -1;
         if (0 != access(fname, F_OK))
             return LOGGER_PERROR("FILE: %s", fname), -1;
-        LOGGER_INFO("FILE: %s", *fn);
     }
     LOGGER_INFO("verifying files...done");
     return 0;
