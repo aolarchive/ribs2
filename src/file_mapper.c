f875a31bcab3cde5b554bcbd932d961ffed4b7f7
Lior Amram <lior@adap.tv>
2013-08-15 17:45:59 -0700

    file_mapper: allow custom init

--- src/file_mapper.c
+++ src/file_mapper.c
@@ -26,26 +26,13 @@
 #include <sys/mman.h>
 
 int file_mapper_init(struct file_mapper *fm, const char *filename) {
-    if (0 > file_mapper_free(fm))
-        return -1;
-    int fd = open(filename, O_RDONLY | O_CLOEXEC);
-    if (fd < 0)
-        return LOGGER_PERROR_FUNC("open: %s", filename), -1;
-    struct stat st;
-    if (0 > fstat(fd, &st))
-        return LOGGER_PERROR_FUNC("fstat %s", filename), close(fd), -1;
-    fm->size = st.st_size;
-    fm->mem = st.st_size ? (char *)mmap(NULL, RIBS_VM_ALIGN(fm->size), PROT_READ, MAP_SHARED, fd, 0) : NULL;
-    close(fd);
-    if (MAP_FAILED == fm->mem)
-        return LOGGER_PERROR_FUNC("mmap %s", filename), fm->mem = NULL, -1;
-    return 0;
+    return file_mapper_init2(fm, filename, 0, O_RDONLY | O_CLOEXEC, PROT_READ, MAP_SHARED);
 }
 
-int file_mapper_init_rw(struct file_mapper *fm, const char *filename, size_t size) {
+int file_mapper_init2(struct file_mapper *fm, const char *filename, size_t size, int flags, int mmap_prot, int mmap_flags) {
     if (0 > file_mapper_free(fm))
         return -1;
-    int fd = open(filename, O_RDWR | O_CREAT | O_CLOEXEC, 0644);
+    int fd = open(filename, flags);
     if (fd < 0)
         return LOGGER_PERROR_FUNC("open: %s", filename), -1;
     if (0 == size) {
@@ -58,11 +45,15 @@ int file_mapper_init_rw(struct file_mapper *fm, const char *filename, size_t siz
             return LOGGER_PERROR("ftruncate: %s", filename), close(fd), -1;
     }
     fm->size = size;
-    fm->mem = size ? (char *)mmap(NULL, RIBS_VM_ALIGN(fm->size), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0) : NULL;
+    fm->mem = size ? (char *)mmap(NULL, RIBS_VM_ALIGN(size), mmap_prot, mmap_flags, fd, 0) : NULL;
     close(fd);
     if (MAP_FAILED == fm->mem)
         return LOGGER_PERROR_FUNC("mmap %s", filename), fm->mem = NULL, -1;
     return 0;
+}
+
+int file_mapper_init_rw(struct file_mapper *fm, const char *filename, size_t size) {
+    return file_mapper_init2(fm, filename, size, O_RDWR | O_CREAT | O_CLOEXEC, PROT_READ | PROT_WRITE, MAP_SHARED);
 }
 
 int file_mapper_free(struct file_mapper *fm) {
