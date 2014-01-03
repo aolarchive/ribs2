63150f055eb4345649707df99eb02e78d2450797
Michael Mior <mmior@uwaterloo.ca>
2014-01-03 08:19:41 -0500

    Use mysql_config for MySQL compilation options

--- examples/mydump/src/mydumper.mk
+++ examples/mydump/src/mydumper.mk
@@ -5,13 +5,13 @@ SRC=mydumper.c     # list of source files
 RIBIFY=libmysqlclient.a  # ribify this needed library
 
 # we need to tell gcc where to find the include files of ribs2
-CFLAGS+= -I ../../../include
+CFLAGS+= -I ../../../include `mysql_config --include`
 
 # we need to tell the linker where to find ribs2 libraries
 LDFLAGS+= -L ../../../lib -lribs2 -lribs2_mysql
 
 # as well as where to find the mysql libraries
-LDFLAGS+=-L../ribified -pthread -lribs2_mysql -lmysqlclient -lssl -lrt \
+LDFLAGS+=-L../ribified -pthread -lribs2_mysql `mysql_config --libs` \
                 -lm -lz -ldl -L../../ribs2/lib -lribs2
 
 include ../../../make/ribs.mk  # include ribs2 make system
